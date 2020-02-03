c54f53c3fd3eb1dc967221783e7084d6fe0ba6bc
Lilian Dias <lilian.dias@accenture.com>
2020-02-02 23:43:03 -0300

    Init Angular App

---
+++ demo-themoviedb/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { MoviedbCardBoxComponent } from './component/moviedb-card-box/moviedb-card-box.component';
+import { MoviedbInfoLeadspaceComponent } from './component/moviedb-info-leadspace/moviedb-info-leadspace.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MoviedbCardBoxComponent,
+    MoviedbInfoLeadspaceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ demo-themoviedb/src/app/component/moviedb-info-leadspace/moviedb-info-leadspace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moviedb-info-leadspace',
+  templateUrl: './moviedb-info-leadspace.component.html',
+  styleUrls: ['./moviedb-info-leadspace.component.scss']
+})
+export class MoviedbInfoLeadspaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
